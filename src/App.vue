d3467c1132476819a52f447dd52d3d047275518c
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-01-21 22:34:20 +0530

    setup app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <component :is="currentView"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Page404 from "./pages/Page404.vue";
+
+const routes = {
+  '/': HelloWorld
+}
 
 export default {
-  name: 'App',
+  data(){
+    return {
+      currentPath: window.location.hash
+    }
+  },
   components: {
     HelloWorld
-  }
+  },
+  computed:{
+    currentView() {
+      //scroll to page top before changing the view
+      window.scrollTo({ top: 0, behavior: 'auto' });
+
+      return routes["/" + this.currentPath.slice(1)] || Page404;
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      // TODO pass a reference to the current menu item
+      this.currentPath = window.location.hash;
+    });
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
