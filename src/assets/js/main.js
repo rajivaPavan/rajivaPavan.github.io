157f6eff69a57d0346c9b2aaded568e76c19135c
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-01-22 18:38:47 +0530

    removed typed effect lib usage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -43,6 +43,7 @@
   /**
    * Navbar links active state on scroll
    */
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -132,22 +133,6 @@
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove()
-    });
-  }
-
-  /**
-   * Hero type effect
-   */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
     });
   }
 
