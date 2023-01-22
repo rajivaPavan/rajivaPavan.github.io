a6524d5a1b26757606d85f6e13078d19aa76ae36
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-01-22 16:34:12 +0530

    updated routing

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,14 @@ const routes = {
   '/': PageHome
 }
 
+const getCurrentPath = ()=>{
+  return window.location.pathname;
+}
+
 export default {
   data(){
     return {
-      currentPath: window.location.hash
+      currentPath: getCurrentPath()
     }
   },
   components: {
@@ -21,17 +25,30 @@ export default {
   },
   computed:{
     currentView() {
+      const currentPath = this.currentPath.slice(1); //remove the leading slash
+
+      if(currentPath.startsWith('#')){
+        //scroll to section with id
+        const section = document.getElementById(currentPath.slice(1)); //remove the leading hash
+        if(section){
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+
       //scroll to page top before changing the view
       window.scrollTo({ top: 0, behavior: 'auto' });
 
-      return routes["/" + this.currentPath.slice(1)] || Page404;
+      return routes["/" + currentPath] || Page404;
     },
   },
   mounted() {
-    window.addEventListener("hashchange", () => {
-      // TODO pass a reference to the current menu item
-      this.currentPath = window.location.hash;
-    });
+    // watch for changes in the current path
+    this.$watch(
+        () => getCurrentPath(),
+        (newValue) => {
+           this.currentPath = newValue;
+        }
+    );
   },
 }
 </script>
