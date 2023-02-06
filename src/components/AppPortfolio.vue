b34f5c493eb6fe65e644ba8d99fe0c804e4fd110
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-02-06 11:08:56 +0530

    moved portfolio images to public folder

--- src/components/AppPortfolio.vue
+++ src/components/AppPortfolio.vue
@@ -22,12 +22,12 @@
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
-            <img src="../assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <img src="../../public/assets/images/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
               <h4>App 1</h4>
               <p>App</p>
               <div class="portfolio-links">
-                <a href="../assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
+                <a href="../../public/assets/images/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
                    class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external"
                    title="Portfolio Details"><i class="bx bx-link"></i></a>
